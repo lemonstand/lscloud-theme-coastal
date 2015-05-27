446b30781df870000c33a4fb513115bef677453e
Adam Campbell <adam@hotmeteor.com>
2015-05-27 16:26:28 -0500

    Fixed issue #15
    Fixes issue #15
    Added lemon sync.cfg to gitignore

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 $(document).ready(function() {
     (function($) {
         "use strict";
@@ -30,48 +32,61 @@ $(document).ready(function() {
     $("#modal-minicart").on("mouseleave", ".modal-dialog", function() {
         $("#modal-minicart").modal("hide");
     });
+
     (function($) {
-        var $source = $("#billing-info");
-        var $sourceInput = $source.find(":input:not([type=hidden])");
-        var $target = $("#shipping-info");
-        var $targetInput = $target.find(":input:not([type=hidden])");
+
+        // var $source = $("#billing-info");
+        // var $sourceInput = $source.find(":input:not([type=hidden])");
+
         var _regex = /\[(.*?)\]/;
-        var _ev = "keyup keypress blur change";
-        $(document).on("click.address update:address", ".js-mirrordata", function() {
-            if ($(this).is(":checked")) {
-                mirrorAll();
-                $sourceInput.on(_ev, mirrorField);
-            } else {
-                $sourceInput.off(_ev, mirrorField);
+        var _ev = "keyup blur change";
+
+        $(document).on(_ev, '#billing-info :input:not([type=hidden])', function(ev) {
+
+            var $chk = $(document).find('.js-mirrordata');
+
+            if (!$chk.is(':checked')) {
+                return;
             }
+
+            mirrorField.apply(ev.target);
+
+            // mirrorAll();
+            return false;
         });
-        $(".js-mirrordata").trigger("update:address");
-        function mirrorAll() {
-            $sourceInput.each(function() {
-                mirrorField.apply(this);
-            });
-        }
+
         function mirrorField() {
+
             if (!$(this).attr("name")) {
                 return;
             }
+
             var $el = $(this);
             var mirrorVal = $el.val();
             var nameMatch = $el.attr("name").match(_regex);
+            var $target = $("#shipping-info");
+            var $targetInput = $target.find(":input:not([type=hidden])");
+
             if (!nameMatch) {
                 return;
             }
+
             nameMatch = nameMatch[1];
+
             var re = new RegExp(nameMatch, "g");
             var $targetEl = $targetInput.filter(function() {
                 return this.name.match(re);
             });
+
             $targetEl.val(mirrorVal);
+
             if ($el[0].id === "billing_country") {
                 $targetEl.trigger("change");
             }
         }
+
     })(jQuery);
+
     $("#cart-content").on("keydown", "input#coupon", function(ev) {
         if (ev.which === 13) {
             $(this).sendRequest("shop:cart", {
